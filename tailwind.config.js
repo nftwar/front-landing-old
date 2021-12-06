8908e6849b59d2928c7229e0bc209ad8107e2c83
PINOT <pinot.kim@kakao.com>
2021-12-06 19:39:21 +0900

    Add typescript and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+      "./pages/**/*.{ts,tsx}",
+      "./containers/**/*.{ts,tsx}",
+      "./components/**/*.{ts,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
