74dd1840c5aa9b4e10de69671ed8ca7964cec450
PINOT <pinot.kim@kakao.com>
2021-12-07 15:44:13 +0900

    Add interface page

---
+++ pages/interface.tsx
@@ -0,0 +1,14 @@
+function InterfacePage() {
+	return (
+		<main className={"my-[30px] bg-[#F2F2F2] px-[28px] py-[44px]"}>
+			<section className={"container mx-auto bg-white border-solid border-[1px] border-black rounded-[8px]"}>
+				<h1 className={"px-[24px] py-[18px] font-bold text-[24px]"}>Interface</h1>
+				<article className={"flex items-center justify-center min-h-[500px]"}>
+					Unity WebGL Player
+				</article>
+			</section>
+		</main>
+	);
+}
+
+export default InterfacePage;
