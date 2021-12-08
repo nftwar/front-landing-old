52f6996e0db83ba4fdbb7955df8941dd8a64c374
benjioh5 <benjioh5@gmail.com>
2021-12-08 21:30:40 +0900

    remove .gz

--- components/unity.tsx
+++ components/unity.tsx
@@ -3,9 +3,9 @@ import Unity, { UnityContext } from "react-unity-webgl";
 export function UnityView() {
     const unityContext = new UnityContext({
 		loaderUrl: "assets/webgl/Build.loader.js",
-		dataUrl: "assets/webgl/Build.data.gz",
-		frameworkUrl: "assets/webgl/Build.framework.js.gz",
-		codeUrl: "assets/webgl/Build.wasm.gz",
+		dataUrl: "assets/webgl/Build.data",
+		frameworkUrl: "assets/webgl/Build.framework.js",
+		codeUrl: "assets/webgl/Build.wasm",
 	});
 
     return <Unity unityContext={unityContext} />
