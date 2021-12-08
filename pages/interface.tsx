a66deab9a4701443d789353e8b74dd625c2a5512
benjioh5 <benjioh5@gmail.com>
2021-12-08 20:31:12 +0900

    add unity webgl

--- pages/interface.tsx
+++ pages/interface.tsx
@@ -1,10 +1,19 @@
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+	loaderUrl: "assets/webgl/Build.loader.js",
+	dataUrl: "assets/webgl/Build.data.gz",
+	frameworkUrl: "assets/webgl/Build.framework.js.gz",
+	codeUrl: "assets/webgl/Build.wasm.gz",
+});
+
 function InterfacePage() {
 	return (
 		<main className={"my-[30px] bg-[#F2F2F2] px-[28px] py-[44px]"}>
 			<section className={"container mx-auto bg-white border-solid border-[1px] border-black rounded-[8px]"}>
 				<h1 className={"px-[24px] py-[18px] font-bold text-[24px]"}>Interface</h1>
 				<article className={"flex items-center justify-center min-h-[500px]"}>
-					Unity WebGL Player
+					<Unity unityContext={unityContext} />;
 				</article>
 			</section>
 		</main>
