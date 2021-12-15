a29fcbf87ce1912cd59bf887f8de74000a559465
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-12-15 12:35:15 +0000

    Prevent path traversal

--- pages/interface.tsx
+++ pages/interface.tsx
@@ -6,7 +6,7 @@ function InterfacePage() {
 			<section className={"bg-gradient-to-br text-white from-[#0D2289] to-[#06093B] grid-span-1 border-solid border-[3px] border-[#c6c6c6] rounded-[8px] p-8 pl-12 text-[20px]"}>
 				<h1 className={"px-[24px] py-[18px] font-bold text-[24px]"}>Interface</h1>
 				<article className={"flex items-center justify-center min-h-[500px]"}>
-					{ process.browser && <UnityView />}
+					{/* { process.browser && <UnityView />} */}
 				</article>
 			</section>
 		</main>
