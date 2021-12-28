f7676041709793ddd8cbe001269c6f0548fc44ca
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-12-28 08:53:34 +0000

    Add contents at market page

--- pages/market.tsx
+++ pages/market.tsx
@@ -1,9 +1,14 @@
-function Market() {
-    return (
-        <main>
-            
-        </main>
-    );
+function MarketPage() {
+	return (
+		<main className={"bg-[#303030] px-[28px] py-[44px]"}>
+			<section className={"bg-gradient-to-br text-white from-[#0D2289] to-[#06093B] grid-span-1 border-solid border-[3px] border-[#c6c6c6] rounded-[8px] p-8 pl-12 text-[20px]"}>
+				<h1 className={"px-[24px] py-[18px] font-bold text-[24px]"}>Market</h1>
+				<article className={"flex items-center justify-center min-h-[500px]"}>
+					Comming Soon!
+				</article>
+			</section>
+		</main>
+	);
 }
 
-export default Market;
+export default MarketPage;
