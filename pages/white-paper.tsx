b8ea080a6a16282bbbb31ea4b068f2122357bf7c
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-12-28 08:47:29 +0000

    Modify text at white-paper page

--- pages/white-paper.tsx
+++ pages/white-paper.tsx
@@ -4,7 +4,7 @@ function WhitePaperPage() {
 			<section className={"bg-gradient-to-br text-white from-[#0D2289] to-[#06093B] grid-span-1 border-solid border-[3px] border-[#c6c6c6] rounded-[8px] p-8 pl-12 text-[20px]"}>
 				<h1 className={"px-[24px] py-[18px] font-bold text-[24px]"}>Whitepaper</h1>
 				<article className={"flex items-center justify-center min-h-[500px]"}>
-					적당한 텍스트
+					Comming Soon!
 				</article>
 			</section>
 		</main>
