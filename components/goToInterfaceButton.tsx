f356a23a88a7b01684d7b24912ba8baa30c1d823
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-12-28 12:14:05 +0000

    Modify link at GoToInterfaceButton

--- components/goToInterfaceButton.tsx
+++ components/goToInterfaceButton.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export function GoToInterfaceButton() {
 	return (
-		<button className={"inline-block px-12 py-2.5 rounded-[5px] min-w-[140px] bg-transparent border-solid border-[2px] border-[#FFAA00] hover:bg-[#FFAA00] text-[#FFAA00] hover:text-white"}>
-			<Link href={"/interface"}>
-				<a>Go to Interface</a>
-			</Link>
-		</button>
+		<Link href={"/interface"}>
+			<button className={"inline-block px-12 py-2.5 rounded-[5px] min-w-[140px] bg-transparent border-solid border-[2px] border-[#FFAA00] hover:bg-[#FFAA00] text-[#FFAA00] hover:text-white"}>
+				Go to Interface
+			</button>
+		</Link>
 	);
 }
